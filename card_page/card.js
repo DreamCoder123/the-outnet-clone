20442b89c68857c39630382f6fb6a84379c78a87
prashantshetti07 <prashantshetti007@gmail.com>
2022-05-07 17:56:57 +0530

    card is added

---
+++ card_page/card.js
@@ -0,0 +1,252 @@
+var fashionData = [
+    {
+      avatar:
+        "https://rukminim2.flixcart.com/image/495/594/l1dwknk0/top/t/e/h/m-85-fab-star-original-imagcyygsyjfzzy5.jpeg?q=50",
+        brand : "Fab Star",
+      name: "cloth",
+      price: "$450",
+      offer : "%50 off"
+     
+       },
+    {
+        avatar:
+        "https://www.theoutnet.com/variants/images/33258524072427197/R/w358_q80.jpg",
+      
+        brand : "Fab Star",
+      name: "shoes",
+      price: "$450",
+      offer : "%50 off"
+      
+    },
+    {
+        avatar:
+        "https://rukminim2.flixcart.com/image/495/594/kw6pw280/shopsy-sling-bag/i/n/w/peach-women-sling-bag-regular-size-pu-sh-isr-sb12-regular-size-original-imag8utfxreqhuhc.jpeg?q=50",
+        brand : "Snappy",
+      name: "bag",
+      price: "$450",
+      offer : "%50 off"
+    },
+    {
+        avatar:
+        "https://rukminim2.flixcart.com/image/495/594/kvcpn680/kurta/h/f/d/m-new-crepe-printed-straight-kurti-for-women-girls-threadhunter-original-imag89xxszmgsxsf.jpeg?q=50",
+        brand : "Snappy",
+      name: "cloth",
+      price: "$450",
+      offer : "%50 off"
+    
+    },
+    {
+        avatar:
+        "https://rukminim2.flixcart.com/image/495/594/kua4r680/hand-messenger-bag/k/2/o/lwhb02253lightpink-lwhb02253lightpink-shoulder-bag-lino-perros-original-imag7f42ceebzzhw.jpeg?q=50",
+        brand : "Fab Star",
+      name: "bag",
+      price: "$450",
+      offer : "%50 off"
+    },
+    {
+        avatar:
+        "https://rukminim2.flixcart.com/image/495/594/kxaq7ww0/fabric/9/k/3/yes-2-5-m-semi-stitched-2-m-2-5-m-5278-32-207-desiqulture-original-imag9saasxdzupug.jpeg?q=50",
+        brand : "Snappy",
+      name: "cloth ",
+      price: "$450",
+      offer : "%50 off"
+    },
+    {
+        avatar:
+        "https://www.theoutnet.com/variants/images/33258524072190085/R/w358_q80.jpg",
+        brand : "Fab Star",
+      name: "shoes",
+      price: "$450",
+      offer : "%50 off"
+    },
+    {
+        avatar:
+        "https://rukminim2.flixcart.com/image/495/594/ky1vl3k0/kurta/b/c/m/xl-02-long-kurti-set-golden-fabric-original-imagadf4tpvqky7e.jpeg?q=50",
+        brand : "Snappy",
+      name: "cloth",
+      price: "$450",
+      offer : "%50 off"
+    },
+    {
+        avatar:
+        "https://rukminim2.flixcart.com/image/495/594/kcuug7k0/kurta/0/s/8/m-dffo7-m-lala-fashion-original-imaftvjaufxnnwhe.jpeg?q=50",
+        brand : "Fab Star",
+      name: "cloth",
+      price: "$450",
+      offer : "%50 off"
+    },
+    {
+        avatar:
+        "https://rukminim2.flixcart.com/image/432/518/kw6pw280/shopsy-sling-bag/v/7/p/green-women-sling-bag-regular-size-pu-sh-isr-sb12-regular-size-original-imag8utff5zhmnwz.jpeg?q=70",
+        brand : "Snappy",
+      name: "bag ",
+      price: "$450",
+      offer : "%50 off"
+    },
+    {
+        avatar:
+        "https://www.theoutnet.com/variants/images/33258524072398081/R/w358_q80.jpg",
+        brand : "Fab Star",
+      name: "shoes",
+      price: "$450",
+      offer : "%50 off"
+    },
+    {
+        avatar:
+        "https://rukminim2.flixcart.com/image/432/518/l27wtjk0/sling-bag/q/4/t/silver-stylish-mini-sling-bag-for-women-tnz-sb4-sling-bag-original-imagdm3yag2rddv5.jpeg?q=70",
+        brand : "Genie",
+      name: "bag",
+      price: "$450",
+      offer : "%50 off"
+    },
+    {
+        avatar:
+        "https://rukminim2.flixcart.com/image/570/684/kzygpzk0/kurta/t/p/k/xl-anu1153-annu-paridhan-original-imagburpu7szszf3.jpeg?q=50",
+        brand : "Fab Star",
+      name: "cloth",
+      price: "$450",
+      offer : "%50 off"
+    },
+    {
+        avatar:
+        "https://www.theoutnet.com/variants/images/33258524072159269/R/w358_q80.jpg",
+        brand : "Snappy",
+      name: "shoes",
+      price: "$450",
+      offer : "%50 off"
+    },
+    {
+        avatar:
+        "https://rukminim2.flixcart.com/image/570/684/kd7pevk0/shirt/7/g/b/s-ttsh000905-tokyo-talkies-original-imafu5tz94kzs4jr.jpeg?q=50",
+        brand : "Fab Star",
+      name: "cloth",
+      price: "$450",
+      offer : "%50 off"
+    },
+    
+    {
+        avatar:
+        "https://rukminim2.flixcart.com/image/495/594/kxxl9jk0/sling-bag/g/h/4/women-s-leatherette-sling-bag-black-black-fgo-342-1-25-fgo-342-original-imagaa8pxzn7ajyh.jpeg?q=50",
+        brand : "Snappy",
+      name: "bag",
+      price: "$450",
+      offer : "%50 off"
+    },
+    {
+        avatar:
+        "https://www.theoutnet.com/variants/images/29419655932328347/R/w358_q80.jpg",
+        brand : "Fab Star",
+      name: "shoes",
+      price: "$450",
+      offer : "%50 off"
+    },
+    {
+        avatar:
+        "https://rukminim2.flixcart.com/image/570/684/klphn680/top/n/5/x/xl-regular-top-fab-star-original-imagyry2geb5ztzq.jpeg?q=50",
+        brand : "Snappy",
+      name: "cloth",
+      price: "$450",
+      offer : "%50 off"
+    },
+    {
+        avatar:
+        "https://rukminim2.flixcart.com/image/495/594/kyvvtzk0/hand-messenger-bag/k/8/z/ahbgfrgbs40001-32-ahbgfrgbs40001-shoulder-bag-allen-solly-28-original-imagbysznh8hxnhd.jpeg?q=50",
+        brand : "Fab Star",
+      name: "bag",
+      price: "$450",
+      offer : "%50 off"
+    },
+    {
+        avatar:
+        "https://rukminim2.flixcart.com/image/570/684/km0x5zk0/top/j/z/o/m-178tkr2901-selvia-original-imagfyqfytkkfpkn.jpeg?q=50",
+        brand : "Fab Star",
+      name: "cloth",
+      price: "$450",
+      offer : "%50 off"
+    },
+    {
+        avatar:
+        "https://www.theoutnet.com/variants/images/42247633208302683/R/w358_q80.jpg",
+        brand : "Snappy",
+      name: "shoes",
+      price: "$450",
+      offer : "%50 off"
+    },
+    
+    
+  ];
+
+  //fashionArr=JSON.parse(localStorage.getItem("fashionStorage"))||[]
+  fashionArr=[]
+//displayData(fashionArr)
+//console.log (fashionArr)
+
+
+
+//function displayData(fashionArr){
+  fashionData.forEach(function (elem) {
+    //console.log (elem.name)
+    var box2 = document.createElement("div");
+    var box1 = document.createElement("a")
+    box1.setAttribute("href","raj.html")
+
+    var box = document.createElement("div");
+    var fashion_avatar = document.createElement("img");
+    fashion_avatar.setAttribute("src", elem.avatar);
+     var  fashion_brand = document.createElement("h3");
+    fashion_brand.innerText = elem.brand;
+    var fashion_name = document.createElement("p");
+    fashion_name.innerText = elem.name;
+    var fashion_price = document.createElement("p");
+    fashion_price.innerText = elem.price;
+    fashion_price.style.color = "red"
+    var fashion_offer = document.createElement("p");
+    fashion_offer.innerText = elem.offer;
+    
+    box.append(fashion_avatar,  fashion_brand, fashion_name , fashion_price, fashion_offer);
+    box1.append(box)
+    box2.append(box1)
+
+    document.querySelector("#container").append(box2);
+    //console.log (elem)
+
+    fashionArr.push(elem)
+    localStorage.setItem("fashionStorage",JSON.stringify(fashionArr))
+
+  
+
+
+  })
+//}
+
+
+//filterArr = JSON.parse(localStorage.getItem("fashionStorage"))
+//function handleFilter(){
+ // var selected = document.getElementById("filterVenue").value
+ //console.log(selected)
+ //console.log (filterArr)
+  
+ //var filteredlist = filterArr.filter(function(elem){
+ 
+    //if (elem.name==selected){
+     // console.log(elem)
+      //  return elem
+    //}
+   
+ // })
+  //displayData(filteredlist)
+
+    //console.log (filteredlist)
+    //console.log (selected)
+
+ // }
+
+
+  
+
+   
+
+
+
+ 
+  
